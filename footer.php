67cd348627d4e4c40a90ce5d1797d2a1f90f7a38
Jennifer Pike <jpike@csis.org>
2026-07-18 10:50:22 -0700

    changed where slate blue goes

--- footer.php
+++ footer.php
@@ -10,9 +10,9 @@
 	<div class="site-footer">
 			<?php get_sidebar(); ?>
 			<div class="socials">
-				<a class="google-reviews" href="https://www.google.com/search?q=pike+x+digital&rlz=1C5CHFA_enGB1008GB1008&oq=&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGDkyBggCECMYJzIHCAMQABiABDIGCAQQRRg9MgYIBRBFGEEyBggGEEUYQTIGCAcQRRg80gEIMjkxOWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8&sei=XsGxabGiN7yQm9cP1-a1EA#lrd=0x48760362ff2987eb:0x8fd7e37fac7dcec6,1,,,," target="_blank"></a>
-				<a class="instagram" href="https://www.instagram.com/pikexdigital/" target="_blank"></a>
-				<a class="facebook" href="https://www.facebook.com/pikexdigital/" target="_blank"></a>
+				<a class="google-reviews"></a>
+				<a class="instagram"></a>
+				<a class="facebook"></a>
 			</div>
 	</div>
 	<div class="site-footer__legal">&copy; <?php echo date("Y"); ?> <?php echo get_bloginfo( 'name' ); ?> | Site by <a href="https://pikexdigital.com" target="_blank">Pike x Digital</a> for Hello Digital.</div>
